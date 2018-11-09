4ea540290285c3f732104aa496b01fbd0446b43b
Jason Slavin <jslavin@marketo.com>
2018-11-08 18:48:59 -0800

    This componenet now can take user input, and then place that user input on the page. Things still need to be exact

--- src/js/components/container/dashboard.js
+++ src/js/components/container/dashboard.js
@@ -13,11 +13,12 @@ class Dashboard extends Component {
             textTile: "Some Text Goes Here",
             data: [],
             users: [],
-            userQueryParams: [],
             url: '',
             student: '',
-            keyword: '',
+            keywords: '',
         };
+        this.updateQueries = this.updateQueries.bind(this);
+        this.setDataForCall = this.setDataForCall.bind(this);
     }
 
     createUserList (data) {
@@ -35,10 +36,44 @@ class Dashboard extends Component {
         });
     }
 
-    retrieveUserQueryParams (event) {
-        event.preventDefault();
-        this.setState({userQueryParams: [this.state.url, this.state.student, this.state.keyword]});
-        console.log(this.state, 'state');
+    parseRequestParams () {
+        let paramObject = {limit: 200};
+        this.state.url !== '' ? paramObject['url'] = this.state.url : undefined;
+        this.state.student !== '' ? paramObject['user'] = this.state.student : undefined;
+        this.state.keywords !== '' ? paramObject['quote'] = this.state.keywords : undefined;
+
+        return paramObject;
+    }
+
+
+    setDataForCall () {
+
+        axios.request({
+            url: `https://hypothes.is/api/search`,
+            method: 'get',
+            // params: {
+            //     url: 'http://teaching.lfhanley.net/english528sp18/texts/edna-st-vincent-millay/',
+            //     // user: 'lydiajen@hypothes.is',
+            //     limit: 200,
+            //     password: 'Basic 6879-UpkRG4InzmDMO5jsOyMDvMTFltIlCHNLG-j6gpex2Ok',
+            //     quote: "making friends",
+            // },
+            params: this.parseRequestParams(),
+        }).then((response) => {
+            console.log(response);
+            this.setState({
+                data: response.data.rows,
+                users: this.createUserList(response.data.rows),
+            });
+            console.log(this.state);
+        }).catch((error) => {
+            console.log('error', error);
+        });
+    }
+
+    updateQueries (value, name) {
+        this.setState({[name]: value});
+        console.log(this.state);
     }
 
     componentDidMount () {
@@ -68,8 +103,16 @@ class Dashboard extends Component {
         return (
             <div className={'main-dashboard-wrapper'}>
                 <p>{this.state.textTile}</p>
-                <section className={'user-input-area'}></section>
-                <QuerySetter retrieveParams={this.retrieveUserQueryParams}/>
+                <section className={'user-input-area'}>
+                    <QuerySetter
+                        retrieveParams={this.retrieveUserQueryParams}
+                        updateQueries={this.updateQueries}
+                        url={this.state.url}
+                        student={this.state.student}
+                        keywords={this.state.keywords}
+                    />
+                    <button type={"submit"} onClick={this.setDataForCall}>Submit</button>
+                </section>
                 <div className={'dashboard-widget-wrapper'}>
                     <AnnotationBrowser data={this.state.data}/>
                     <div className={'graphics-wrapper'}>
--- src/js/components/user-input/query-setter.js
+++ src/js/components/user-input/query-setter.js
@@ -4,36 +4,27 @@ import "./query-setter.scss";
 class QuerySetter extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            url: '',
-            student: '',
-            keywords: '',
-        };
-        this.handleSubmit = this.props.retrieveParams.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange (event) {
-        let inputName = event.target.name;
-        let inputValue = event.target.value;
-        this.setState({[inputName]: inputValue});
-    }
-
-    componentDidMount () {
-        return true;
+        this.props.updateQueries(event.target.value, event.target.name);
     }
 
     render() {
+        const url = this.props.url;
+        const student = this.props.student;
+        const keywords = this.props.keywords;
+
         return (
             <div className={'query-setter'}>
-                <form ref={"form"} onSubmit={this.props.handleSubmit}>
+                <form ref={"form"} onSubmit={this.handleSubmit}>
                     <label htmlFor="url">URL</label>
-                    <input name={"url"} type="text" value={this.state.url} onChange={this.handleChange}/>
+                    <input name={"url"} type="text" value={url} onChange={this.handleChange}/>
                     <label htmlFor="student">Student</label>
-                    <input id="student" name={"student"} type="text" value={this.state.student} onChange={this.handleChange}/>
+                    <input id="student" name={"student"} type="text" value={student} onChange={this.handleChange}/>
                     <label htmlFor="keywords">Keywords / Phrase</label>
-                    <input id="keywords" name={"keywords"} type="text" value={this.state.keywords} onChange={this.handleChange}/>
-                    <button type={'submit'}>Submit Query</button>
+                    <input id="keywords" name={"keywords"} type="text" value={keywords} onChange={this.handleChange}/>
                 </form>
             </div>
         );
